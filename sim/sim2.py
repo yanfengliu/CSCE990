be35641f275013c91cf1004b9763488798faca6f
Yanfeng Liu <yanfengliux@gmail.com>
2019-11-17 21:54:53 -0600

    sim2 not ideal; improved sim1 with random exploration

--- sim/sim2.py
+++ sim/sim2.py
@@ -42,74 +42,59 @@ def get_dist(image, robot_coord, angle_vector):
     return dist, col_old, row_old
 
 
-class MotionPlanner():
-    def __init__(self, max_random):
-        self.max_random = max_random
-        self.counter = 0
-
-    def majority_vote(self, dists, mode, override_random_counter=False):
-        num_dist = len(dists)
-        choices = np.linspace(0, num_dist-1, num_dist)
-        dist_groups = []
-        dist_group = []
-        choice_groups = []
-        choice_group = []
-        for i in range(num_dist):
-            dist = dists[i]
-            if dist > min_dist:
-                dist_group.append(dist)
-                choice_group.append(i)
-            elif len(dist_group) > 0:
-                dist_groups.append(dist_group)
-                choice_groups.append(choice_group)
-                dist_group = []
-                choice_group = []
-        if len(dist_group) > 0:
+def majority_vote_weighted_sum(dists, angle_vectors):
+    num_dist = len(dists)
+    choices = np.linspace(0, num_dist-1, num_dist)
+    dist_groups = []
+    dist_group = []
+    choice_groups = []
+    choice_group = []
+    for i in range(num_dist):
+        dist = dists[i]
+        if dist > min_dist:
+            dist_group.append(dist)
+            choice_group.append(i)
+        elif len(dist_group) > 0:
             dist_groups.append(dist_group)
             choice_groups.append(choice_group)
-        votes = []
-        for dist_group in dist_groups:
-            votes.append(len(dist_group))
-        votes = np.array(votes)
-        if len(votes) > 0:
-            choice = np.argmax(votes)
-            target_dist_group = dist_groups[choice]
-            target_choice_group = choice_groups[choice]
-            if mode == 'max':
-                idx = np.argmax(target_dist_group)
-            elif mode == 'random':
-                if not override_random_counter:
-                    if self.counter <= self.max_random:
-                        self.counter = 0
-                        idx = np.argmax(target_dist_group)
-                    else:
-                        self.counter += 1
-                        idx = np.random.randint(low = 0, high=len(target_dist_group))
-                else:
-                    idx = np.random.randint(low = 0, high=len(target_dist_group))
-            direction = target_choice_group[idx]
-        else:
-            direction = -1
-        return direction
+            dist_group = []
+            choice_group = []
+    if len(dist_group) > 0:
+        dist_groups.append(dist_group)
+        choice_groups.append(choice_group)
+    votes = []
+    for dist_group in dist_groups:
+        votes.append(len(dist_group))
+    votes = np.array(votes)
+    if len(votes) > 0:
+        choice = np.argmax(votes)
+        target_dist_group = dist_groups[choice]
+        target_choice_group = choice_groups[choice]
+        normalized_dist = target_dist_group / np.sum(target_dist_group)
+        target_choice_group = np.array(target_choice_group)
+        new_robot_angle = int(np.sum(target_choice_group * normalized_dist))
+        normalized_dist = np.expand_dims(normalized_dist, axis=-1)
+        target_angle_vectors = angle_vectors[target_choice_group]
+        direction = normalized_dist * target_angle_vectors
+        direction = np.sum(direction, 0)
+        return direction, new_robot_angle
+    else:
+        return None
 
 # constants
 img_size = 500
 robot_size = 5
-num_step = 2000
+num_step = 1000
 num_angles = 16
-max_random = 100
-min_obstacle = 4
 max_obstacle = 8
-min_dist = 25
-num_obstacle = np.random.randint(min_obstacle, max_obstacle)
+min_dist = 100
+num_obstacle = np.random.randint(1, max_obstacle)
 
 BACKGROUND_COLOR = (50, 50, 50)
 OBSTACLE_COLOR = (255, 255, 255)
 ROBOT_COLOR = (0, 255, 0)
 DISTANCE_COLOR = (255, 213, 3)
 WARNING_COLOR = (255, 0, 0)
-
-mp = MotionPlanner(max_random)
 
 # NOTE: nparray is (row, col), ImageDraw is (col, row)
 
@@ -147,7 +132,7 @@ angle_list = np.linspace(0, num_angles-1, num_angles)
 
 now = datetime.now()
 dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
-out = cv2.VideoWriter(f'simulation_v1_{dt_string}.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (img_size, img_size))
+out = cv2.VideoWriter(f'simulation_v2_{dt_string}.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (img_size, img_size))
 
 for i in range(num_step):
     print(i)
@@ -186,24 +171,23 @@ for i in range(num_step):
     dist_idx = dist_idx % num_angles
     dist_idx = dist_idx.astype(np.int)
     visible_dists = dist_map[dist_idx]
-    available_angle_vectors = angle_vectors[dist_idx]
-    available_angle_list = angle_list[dist_idx]
+
     [col_robot, row_robot] = robot_coord
     for j in dist_idx:
         col_end, row_end = end_points[j]
         draw.line((col_robot, row_robot, col_end, row_end), fill=DISTANCE_COLOR, width = 3)
         if dist_map[j] <= min_dist:
             draw.line((col_robot, row_robot, col_end, row_end), fill=WARNING_COLOR, width = 3)
-    step_choice = mp.majority_vote(visible_dists, 'random')
-    if step_choice != -1:
-        robot_angle = available_angle_list[step_choice]
-        step_vector = available_angle_vectors[step_choice]
+    
+    result = majority_vote_weighted_sum(visible_dists, angle_vectors)
+    if result is not None:
+        step_vector, d_angle = result
+        robot_angle += d_angle - 2
     else:
-        step_choice = mp.majority_vote(dist_map, 'random', True)
-        robot_angle = angle_list[step_choice]
-        step_vector = angle_vectors[step_choice]
+        step_vector, robot_angle = majority_vote_weighted_sum(dist_map, angle_vectors)
     d_col, d_row = step_vector
-    robot_coord = [col_robot + d_col, row_robot + d_row]
+    robot_coord = [int(col_robot + d_col), int(row_robot + d_row)]
+
     image = np.asarray(board)
     image = np.copy(image)
     image = image.astype(np.uint8)
