d0ded309d101f835cb7f928142e41eb372062845
Yanfeng Liu <yanfengliux@gmail.com>
2019-12-13 00:06:55 -0600

    nimbus test

--- util.py
+++ util.py
@@ -7,9 +7,14 @@ import tensorflow as tf
 CMAP_DEFAULT = 'gray'
 
 
-def prep_image_for_model(image):
+def resize_img(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (416, 128), interpolation=cv2.INTER_LINEAR)
+    return image
+
+
+def prep_image_for_model(image):
+
     image = image.astype(np.float32) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
@@ -33,18 +38,19 @@ def gray2rgb(im, cmap=CMAP_DEFAULT):
   return result_img
 
 
-def normalize_depth_for_display(depth, pc=95, crop_percent=0, normalizer=None,
-                                cmap=CMAP_DEFAULT):
-  """Converts a depth map to an RGB image."""
-  # Convert to disparity.
-
+def normalize_depth(depth, normalizer=None, pc=95):
   disp = 1.0 / (depth + 1e-6)
   if normalizer is not None:
     disp /= normalizer
   else:
     disp /= (np.percentile(disp, pc) + 1e-6)
-  disp = np.clip(disp, 0, 1)
-  disp = gray2rgb(disp, cmap='plasma')
+  norm_disp = np.clip(disp, 0, 1)
+  return norm_disp
+
+
+def depth_to_rgb(depth, crop_percent=0, cmap=CMAP_DEFAULT):
+  norm_disp = normalize_depth(depth)
+  disp = gray2rgb(norm_disp, cmap='plasma')
   keep_h = int(disp.shape[0] * (1 - crop_percent))
   disp = disp[:keep_h]
   return disp
--- webcam.py
+++ webcam.py
@@ -1,3 +1,7 @@
+import datetime
+import os
+import time
+
 import cv2
 import numpy as np
 
@@ -5,8 +9,26 @@ import depth_model
 import util
 from params import Params
 
+alpha = 0.5
 height = 128
 width = 416
+h_min = int(0.33 * height)
+h_max = int(0.67 * height)
+xy_pairs = [
+    [(  0, h_min + height), ( 83, h_max + height)],
+    [( 83, h_min + height), (166, h_max + height)],
+    [(166, h_min + height), (249, h_max + height)],
+    [(249, h_min + height), (332, h_max + height)],
+    [(332, h_min + height), (415, h_max + height)]
+]
+xy_pairs_h = [
+    [(  0, h_min + height), ( 83, h_max + height)],
+    [( 83, h_min + height), (166, h_max + height)],
+    [(166, h_min + height), (249, h_max + height)],
+    [(249, h_min + height), (332, h_max + height)],
+    [(332, h_min + height), (415, h_max + height)]
+]
+highlight_color = (255, 255, 255)
 
 FLAGS = Params()
 FLAGS.input_dir = 'input'
@@ -27,17 +49,69 @@ else:
 
 inference_model, sess = depth_model.init_inference_model(FLAGS)
 
+
+def draw_regions(board):
+    for xy_pair in xy_pairs_h:
+        x1y1 = xy_pair[0]
+        x2y2 = xy_pair[1]
+        cv2.rectangle(board, x1y1, x2y2, highlight_color, 2)
+    return board
+
+
+def draw_direction(board, direction):
+    overlay = board.copy()
+    xy_pair = xy_pairs[direction]
+    x1y1 = xy_pair[0]
+    x2y2 = xy_pair[1]
+    overlay = cv2.rectangle(overlay, x1y1, x2y2, highlight_color, -1)
+    output = cv2.addWeighted(overlay, alpha, board, 1 - alpha, 0)
+    return output
+
+
+def get_dists(norm_depth):
+    dists = []
+    for i in range(5):
+        w_min = int(width * i / 5)
+        w_max = int(width * (i+1) / 5)
+        dist = np.mean(norm_depth[h_min:h_max, w_min:w_max])
+        dists.append(dist)
+    return dists
+
+
+def get_direction(dists):
+    # right now there is no laser scanner for true dists so
+    # obstacle avoidance is very basic
+    return np.argmin(dists)
+
+
+ts = time.time()
+time_str = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+img_dir = f'img/{time_str}'
+util.mkdir_if_missing(img_dir)
+count = 0
 while rval:
-    board = np.zeros((2*height, width))
-    rval, image = vc.read()
-    image = util.prep_image_for_model(image)
+    board = np.zeros((2*height, width, 3), dtype=np.uint8)
+    rval, raw_image = vc.read()
+    raw_image = util.resize_img(raw_image)
+    image = util.prep_image_for_model(raw_image)
     depth = inference_model.inference_depth(image, sess)
     depth = np.squeeze(depth)
-    depth = util.normalize_depth_for_display(depth)
-    board[:height, :, :] = image
-    board[height:, :, :] = depth
+    norm_depth = util.normalize_depth(depth)
+    depth_rgb = util.depth_to_rgb(depth)
+    depth_rgb *= 255
+    depth_rgb = depth_rgb.astype(np.uint8)
+    dists = get_dists(norm_depth)
+    direction = get_direction(dists)
+    raw_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
+    depth_bgr = cv2.cvtColor(depth_rgb, cv2.COLOR_RGB2BGR)
+    board[:height, :, :] = raw_image
+    board[height:, :, :] = depth_bgr
+    board = draw_regions(board)
+    board = draw_direction(board, direction)
     cv2.imshow("preview", board)
+    cv2.imwrite(os.path.join(img_dir, f'{count}.png'), board)
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+    count += 1
 cv2.destroyWindow("preview")
